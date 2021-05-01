9256c0d0e237d38c6c64c29bdb7dc45737d5d136
jj6990@gmail.com <jj6990@gmail.com>
2021-05-01 18:56:02 -0500

    removing wordpress cache folder

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -42,6 +42,7 @@ export default function Hero() {
 
     const elements = data.allWpBanner.edges;
     const imgs = data.allWpMediaItem.edges;
+    
 
     console.log(elements);
     const sliders = elements.map((element, i) => {
