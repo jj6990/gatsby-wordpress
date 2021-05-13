32f50529c9e5d3cd7a7d7bc258c8978984488721
jj6990@gmail.com <jj6990@gmail.com>
2021-05-12 23:24:25 -0500

    responsive category swiper slider

--- src/components/categorySlider/CategorySlider.js
+++ src/components/categorySlider/CategorySlider.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react"
+import React, { useLayoutEffect, useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { Link } from 'gatsby'
@@ -13,6 +13,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons'
 
 export default function CategorySlider() {
+    SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
     const data = useStaticQuery(graphql`
     query  {
         allWpProductCategory {
@@ -33,9 +35,46 @@ export default function CategorySlider() {
     }
   `)
 
-    SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
-
+    const [windowWidth, setWindowWidth] = useState(0);
     const categories = data.allWpProductCategory.edges;
+
+    useLayoutEffect(() => {
+        function updateSize() {
+            setWindowWidth(window.innerWidth);
+        }
+        window.addEventListener('resize', updateSize);
+        updateSize();
+        return () => window.removeEventListener('resize', updateSize);
+
+    }, [])
+
+    const slices = () => {
+        if (windowWidth > 1024) {
+            return 5;
+        } else if (windowWidth > 768) {
+            return 3;
+        } else {
+            return 2;
+        }
+    }
+    console.log(slices());
+
+    const categoryCards = categories.map((category, i) => {
+        if (category.node.image) {
+            const categoryImage = getImage(category.node.image.localFile.childImageSharp.gatsbyImageData);
+            return (
+                <SwiperSlide key={i} className='card-container'>
+                    <GatsbyImage image={categoryImage} alt={category.node.name} />
+                    <div className="link-container">
+                        <Link className="link-button" to={category.node.link}>{category.node.name}</Link>
+                    </div>
+                </SwiperSlide>
+            )
+        } else {
+            return false;
+        }
+    })
+
 
     return (
         <CategorySliderStyle>
@@ -44,10 +83,13 @@ export default function CategorySlider() {
                     <h2>Categoria de Productos</h2>
                 </div>
                 <div className="swiper-container" >
-                    <Swiper spaceBetween={10}
-                        slidesPerView={5}
-                        scrollbar={{ draggable: true }} className='swiper-wrapper'>
-                        <SwiperSlide className="utility-card swiper-slide">
+                    <Swiper
+                        spaceBetween={20}
+                        slidesPerView={slices()}
+                        scrollbar={{ draggable: true }}
+                        className='swiper-wrapper'>
+
+                        <SwiperSlide className="utility-card">
                             <div className="card-title-container">
                                 <h3>Tenemos todo lo que necesitas para tu hogar</h3>
                             </div>
@@ -55,23 +97,8 @@ export default function CategorySlider() {
                                 <FontAwesomeIcon icon={faArrowAltCircleRight} />
                             </div>
                         </SwiperSlide>
-                        {categories.map((category, i) => {
-                            if (category.node.image) {
-                                const categoryImage = getImage(category.node.image.localFile.childImageSharp.gatsbyImageData);
-                                return (
-                                    <SwiperSlide key={i} className='swiper-slide'>
-                                        <GatsbyImage image={categoryImage} alt={category.node.name} />
-                                        <div className="link-container">
-                                            <Link className="link-button" to={category.node.link}>{category.node.name}</Link>
-                                        </div>
-                                    </SwiperSlide>
-                                )
-                            } else {
-                                return false;
-                            }
-                        }
-                        )
-                        }
+
+                        {categoryCards}
                     </Swiper>
                 </div>
             </FullWidthCont>
--- src/styles/categorySlider/CategorySliderStyle.js
+++ src/styles/categorySlider/CategorySliderStyle.js
@@ -1,78 +1,85 @@
-import styled from 'styled-components';
-import device from '../Device';
+import styled from "styled-components";
+import device from "../Device";
 
 const CategorySliderStyle = styled.section`
-    .title-container {
-        display: flex;
-        justify-content:center;
+  .title-container {
+    display: flex;
+    justify-content: center;
+  }
 
-    }
+  h2 {
+    font-size: 32px;
+    text-align: center;
+    text-transform: uppercase;
+  }
 
-    h2 {
-        font-size: 32px;
-        text-align: center;
-        text-transform: uppercase;
-    }
+  .utility-card {
+    background-color: var(--primary-color);
+    color: white;
+    padding: 15px;
+    height: auto;
+  }
 
-    .utility-card {
-        background-color: var(--primary-color);
-        color: white;
-        padding: 15px;
-        height: auto;
-    }
+  .arrow-container {
+    display: flex;
+    justify-content: flex-end;
+  }
 
-    .arrow-container {
-        display: flex;
-        justify-content: flex-end;
-    }
+  h3 {
+    font-size: 20px;
+    text-transform: uppercase;
+  }
 
-    h3 {
-        font-size: 20px;
-        text-transform: uppercase;
-    }
+  .link-button {
+    text-transform: uppercase;
+    background-color: white;
+    border-radius: 4px;
+    text-align: center;
+    padding: 16px;
+    font-size: 18px;
+    word-wrap: break-word;
+  }
 
-    .link-button {
-        text-transform: uppercase;
-        background-color: white;
-        border-radius: 4px;
-        text-align: center;
-        padding: 16px;
-        word-wrap: break-word;
-    }
+  .link-container {
+    display: flex;
+    justify-content: center;
+    cursor: pointer;
+    margin-bottom: 20px; 
+  }
 
-    .link-container {
-        display: flex;
-        justify-content: center;
-        cursor: pointer;
-        position: absolute;
-        bottom: 8%;
-        left: 50%;
-        margin-left: -40%;
-        width: 150px;
-    }
+  .svg-inline--fa.fa-w-16 {
+    width: 30px;
+    height: 30px;
+  }
 
-    .svg-inline--fa.fa-w-16 {
-        width: 30px;
-        height: 30px;
-    }
+  .gatsby-image-wrapper {
+    object-fit: cover;
+    position: absolute;
+    z-index: -1;
+    padding: 15px;
+    width: 88%;
+  }
 
-    .gatsby-image-wrapper {
-        object-fit: cover;
+  .swiper-wrapper {
+    padding-bottom: 20px;
+  }
 
-    }
+  .swiper-slide {
+    height: auto;
+    display: flex;
+    flex-flow: column;
+    justify-content: flex-end;
+    min-height: 225px;
+  }
 
-    .swiper-wrapper {
-        padding-bottom: 20px;
-    }
+  .swiper-scrollbar {
+    background: #f1f1f1;
+    height: 5px;
+  }
 
-    .swiper-scrollbar {
-        background: #f1f1f1;
-        height: 5px;
-    }
-
-    .swiper-scrollbar-drag {
-        background:var(--primary-color);
-    }
+  .swiper-scrollbar-drag {
+    background: var(--primary-color);
+  }
 `;
 
 export default CategorySliderStyle;
